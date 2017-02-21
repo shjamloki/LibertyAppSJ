9b5f3b6c47a681ba6b23171c32414975278a837c
shjamloki <sjamloki@192.168.1.4>
2017-02-21 21:01:59 +0530

    Adding test case

---
+++ test/ExampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ExampleTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test() {
+		System.out.println("Test Executed..");
+		//fail("Not yet implemented");
+		Assert.assertTrue(true);
+	}
+
+}
